a531bd97cc5a5115fb53c8ebb1e93cfd12aaf24b
TheShooter <30504258+TheShooter71@users.noreply.github.com>
2017-12-20 11:32:07 +0100

    Mise à jour du help

--- commands/Sarah/aide.js
+++ commands/Sarah/aide.js
@@ -1,6 +1,6 @@
 function aide(message,prefix,client){
 if (message.content.startsWith(prefix + "aide")) {
-message.channel.send("Je suis un bot de Modération très fluide et très rapide, pour avoir accès à mes commandes, il vous suffit d'avoir la permission ``gerer le serveur``.\nPour m'inviter dans votre serveur, faites ``"+prefix+"invite``\n\n**Mes commandes** :robot:\n\n\n``purge``, ``embed``, ``mute``, ``unmute``, ``kick``, ``ban``, ``warn``, ``seewarns``, ``clearwarns``, ``addrole``, ``rmrole``");
+message.channel.send("Je suis un bot de **modération** très fluide et très rapide, pour avoir accès à mes commandes, il vous suffit d'avoir la permission ``gerer le serveur``.\nPour m'inviter dans votre serveur, faites ``"+prefix+"invite``\n\n**Mes commandes** :robot:\n\n\n``purge``, ``embed``, ``mute``, ``unmute``, ``kick``, ``ban``, ``warn``, ``seewarns``, ``clearwarns``, ``addrole``, ``rmrole``");
     }
 }
     module.exports = aide
